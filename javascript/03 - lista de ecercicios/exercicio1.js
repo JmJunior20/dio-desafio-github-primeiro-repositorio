5fa3195b5646b2c8c1a7731f095598ac5e350633
JmJunior20 <jmjuniormonteiro@gmail.com>
2022-10-27 17:05:08 -0300

    Exercicio 2

--- javascript/03 - lista de ecercicios/exercicio1.js
+++ javascript/03 - lista de ecercicios/exercicio1.js
@@ -6,17 +6,17 @@
     - Média entre 5 e 7, recuperação;
     - Média menor que 5, reprovação;
 */
-const nota1 = 6;
+const nota1 = 7;
 const nota2 = 7;
 const nota3 = 7;
 
 const media = (nota1 + nota2 + nota3) / 3;
 
-console.log(media.toFixed(2));
+console.log(media.toFixed(1));
 
 if (media > 7) {
   console.log("Passou de semestre!");
-} else if ((media > 5) & (media <= 7)) {
+} else if ((media >= 5) && (media <= 7)) {
   console.log("Recuperação!");
 } else {
   console.log("Reprovado!");
--- javascript/03 - lista de ecercicios/exercicio2.js
+++ javascript/03 - lista de ecercicios/exercicio2.js
@@ -10,3 +10,20 @@
     - Entre 30 e 40 Obeso;
     - Acima de 40 Obsesidade Grave;
 */
+const peso = 70;
+const altura = 1.68;
+
+const imc = peso / Math.pow(altura, 2);
+console.log(imc);
+
+if (imc < 18.5) {
+  console.log("Abaixo do peso!");
+} else if (imc >= 18.5 && imc < 25) {
+  console.log("Peso normal!");
+} else if (imc >= 25 && imc < 30) {
+  console.log("Acima do peso!");
+} else if (imc >= 30 && imc < 40) {
+  console.log("Obeso!");
+} else {
+  console.log("Obsesidade Grave!");
+}
