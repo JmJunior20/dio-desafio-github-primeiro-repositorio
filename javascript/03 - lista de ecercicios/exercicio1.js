435e28c288b10269985ab72bf527fd7624de268e
JmJunior20 <jmjuniormonteiro@gmail.com>
2022-10-27 16:38:00 -0300

    Exercicio 1

--- javascript/03 - lista de ecercicios/exercicio1.js
+++ javascript/03 - lista de ecercicios/exercicio1.js
@@ -2,7 +2,22 @@
     1) Faça um algorítimo que dado as 3 notas tiradas por um aluno em um semestre da faculdade calcule e imprima a sua média e a sua classificação conforme a tabela abaixo.
     Média = (nota 1 + nota 2 + nota 3) / 3;
     Classificação:
-    - Média menor que 5, reprovação;
+    - Média acima de 7, passou de semestre;    
     - Média entre 5 e 7, recuperação;
-    - Média acima de 7, passou de semestre;
+    - Média menor que 5, reprovação;
 */
+const nota1 = 6;
+const nota2 = 7;
+const nota3 = 7;
+
+const media = (nota1 + nota2 + nota3) / 3;
+
+console.log(media.toFixed(2));
+
+if (media > 7) {
+  console.log("Passou de semestre!");
+} else if ((media > 5) & (media <= 7)) {
+  console.log("Recuperação!");
+} else {
+  console.log("Reprovado!");
+}
